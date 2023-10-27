5b54b01f994960511534eca80d3280f634a087a0
Rob Currie <rcurrie@ucsc.edu>
2023-10-27 09:22:57 -0700

    Make the ui a bit more self explanatory

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 import streamlit as st
 
-st.set_page_config(page_title="Braingeneers Search", page_icon="🧠")
+st.set_page_config(page_title="Braingeneers Search", page_icon="🧠", layout="wide")
 
 conn = st.experimental_connection(
     name="search", type="sql", url=os.environ["DATABASE_URL"]
@@ -19,11 +19,16 @@ def handle_click(uuid):
 
 
 with st.sidebar:
-    image = Image.open("logo.png")
-    st.image(image)
+    left, right = st.columns([10, 30])
+    with left:
+        image = Image.open("logo.png")
+        st.image(image)
+    with right:
+        st.title("Braingeneers Search")
 
     query = st.text_input("Query:", label_visibility="collapsed")
 
+    st.write("Experiments found:")
     if query:
         df = conn.query(
             f"select metadata->'uuid' as uuid from experiments where to_tsvector('english', metadata) @@ to_tsquery('{query}:*')",
