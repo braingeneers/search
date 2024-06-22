daf2489af541d90cee205c0288b25cc9b8e06812
Rob Currie <robert.currie@gmail.com>
2024-06-22 09:53:25 -0400

    Clean up ui and add more comments

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 """
-UI for Braingeneers Experiment Repository
+UI for Braingeneers Experiments Respository
+
+Notes:
+@ui.page define routes driving the nicegui ui
+@app.get/head define REST api endpoints via nicegui's underlying fastapi
 """
 
 import os
@@ -18,7 +22,7 @@ import crawl
 from nwb import NWB
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -29,25 +33,34 @@ app.storage.s3 = session.client(
     endpoint_url=os.getenv("S3_ENDPOINT", "https://s3.braingeneers.gi.ucsc.edu"),
 )
 
-# Initialize global crawled database connection
-app.storage.conn = apsw.Connection("data/braingeneers.db")
+# Initialize global crawled sqlite database connection
+app.storage.conn = apsw.Connection(
+    "data/braingeneers.db", flags=apsw.SQLITE_OPEN_READONLY
+)
 
 
-@app.get("/static/{path:path}")
-def static(path: str):
-    """Serve static files with js mimetime corrected for module loading."""
-    if path.endswith(".js"):
-        return FileResponse(f"static/{path}", media_type="application/javascript")
-    else:
-        return FileResponse(f"static/{path}")
+"""
+Access to NRP S3 objects to support viewing datafiles (i.e. nwb files)
+
+There are two approaches supported here:
+
+s3/: Proxy access to files in S3 with cacheing. This ends up doubling the bandwidth
+as the file needs to come from S3 and then to the browser. 
+
+presigned/: Proxy to signed URLs from NRP S3 to object store access for large files
+so that the browser can pull directly form S3 avoiding the double bandwidth.
+Signed URLs are limited to a single HTTP request method i.e. GET, or HEAD,
+but not both. In the browser FS.createLazyFile calls HEAD to get the size
+of the file and then GET, but we can only have a single URL so we handle
+the HEAD here directly via boto3 and then do a redirect to the signed
+URL for subsequent GET requests.
+"""
 
 
-#
-# Proxy to URLs from NRP S3 to object store access for large files
-#
 @app.head("/s3/{path:path}", status_code=status.HTTP_204_NO_CONTENT)
+@app.head("/presigned/{path:path}", status_code=status.HTTP_204_NO_CONTENT)
 async def s3_head(path, response: Response):
-    """Handle HEAD request directly returning ContentLength"""
+    """Handle HEAD request with ContentLength via boto3"""
     logger.info("HEAD: %s", path)
     res = app.storage.s3.head_object(Bucket="braingeneers", Key=path)
     response.headers["Accept-Ranges"] = "bytes"
@@ -58,8 +71,9 @@ async def s3_head(path, response: Response):
     response.headers["Keep-Alive"] = "timeout=5"
 
 
-@lru_cache(maxsize=32)
+@lru_cache(maxsize=64)
 async def get_object_stream(path: str, range: str):
+    """Cache chunks on the assumption users will scan back and forth in time"""
     return app.storage.s3.get_object(Bucket="braingeneers", Key=path, Range=range)[
         "Body"
     ].iter_chunks()
@@ -77,23 +91,6 @@ async def s3_get(path: str, request: Request) -> StreamingResponse:
             "Body"
         ].iter_chunks()
     )
-
-
-#
-# Proxy to presigned URLs from NRP S3 to facilitate direct client/browser
-# to object store access for large files
-#
-@app.head("/presigned/{path:path}", status_code=status.HTTP_204_NO_CONTENT)
-def s3_presigned_head(path, response: Response):
-    """Handle HEAD request directly returning ContentLength"""
-    logger.info("HEAD: %s", path)
-    res = app.storage.s3.head_object(Bucket="braingeneers", Key=path)
-    response.headers["Accept-Ranges"] = "bytes"
-    response.headers["Cache-Control"] = "max-age=3600"
-    response.headers["Connection"] = "keep-alive"
-    response.headers["Content-Length"] = str(res["ContentLength"])
-    response.headers["Content-Type"] = res["ContentType"]
-    response.headers["Keep-Alive"] = "timeout=5"
 
 
 @app.get("/presigned/{path:path}", response_class=RedirectResponse)
@@ -115,11 +112,13 @@ def s3_presigned_get(path: str, request: Request):
         return None
 
 
-#
-# Search UI
-#
+"""
+Search UI
+"""
+
+
 def search(e):
-    """Full text sqlite search with snippets."""
+    """Full text sqlite search with snippet highlighting"""
     app.storage.client["query"].classes("mt-2", remove="mt-24")
     app.storage.client["experiments"].clear()
     with app.storage.client["experiments"]:
@@ -143,14 +142,17 @@ async def home():
         "display: block; margin-left: auto; margin-right: auto; height: 59px; width: 47px"
     )
 
+    # stack-label 'should' hide the label if None, but doesn't work...
     app.storage.client["query"] = (
-        ui.input(placeholder="Search...")
-        .props('autofocus outlined rounded item-aligned input-class="ml-3"')
+        ui.input(label=None, placeholder="Search...")
+        .props(
+            'stack-label dense autofocus outlined rounded item-aligned input-class="ml-3"'
+        )
         .classes("w-96 self-center mt-24 transition-all")
         .on("keydown.enter", search)
     )
 
-    # Count number and dates of experiments indexed
+    # Count and display count and dates of experiments indexed
     count = app.storage.conn.execute("SELECT COUNT(*) FROM experiments;").fetchone()[0]
     oldest = app.storage.conn.execute(
         "SELECT MIN(last_modified) FROM experiments;"
@@ -165,9 +167,11 @@ async def home():
     app.storage.client["experiments"] = ui.list().props("dense separator")
 
 
-#
-# Experiment UI
-#
+"""
+Experiment UI
+"""
+
+
 @lru_cache(maxsize=32)
 def list_objects(path):
     """Returns list of all objects under the given path recursively"""
@@ -183,7 +187,8 @@ def list_objects(path):
 @ui.page("/experiment/{path:path}")
 def experiment(path: str):
     """Single experiment UI with metadata.json and all files listed."""
-    ui.markdown(f"## **Path**: {path}")
+    ui.markdown(f"# {path}")
+    ui.markdown("## objects:")
     paths = list_objects(path)
     with ui.list().props("dense separator"):
         for p in paths:
@@ -194,6 +199,7 @@ def experiment(path: str):
                 )
             else:
                 ui.item(p)
+    ui.markdown("## metadata.json:")
     key = f"{path}/metadata.json"
     res = app.storage.s3.get_object(Bucket="braingeneers", Key=key)
     metadata = json.loads(res.get("Body").read().decode("utf-8"))
@@ -201,9 +207,23 @@ def experiment(path: str):
     editor.run_editor_method("updateProps", {"readOnly": True})
 
 
-#
-# NWB File UI
-#
+"""
+NWB File UI
+
+Note: Experimental and not fully working
+"""
+
+
+@app.get("/static/{path:path}")
+def static(path: str):
+    """Serve static files with js mimetime corrected for module loading."""
+    logger.info("Serving static file: %s", path)
+    if path.endswith(".js"):
+        return FileResponse(f"static/{path}", media_type="application/javascript")
+    else:
+        return FileResponse(f"static/{path}")
+
+
 @ui.page("/nwb/{path:path}")
 def nwb(path: str):
     """Browse contents of NWB file using h5wasm to read directly."""
@@ -216,4 +236,4 @@ def nwb(path: str):
 
 
 # Initialize and start application
-ui.run()
+ui.run(show=False)
